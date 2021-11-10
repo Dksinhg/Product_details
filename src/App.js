4b5e09c32ca7eb48b1d2165d81b04fdafdbd5a9e
Dinesh Kumar <ds8284927561@gmail.com>
2021-11-11 02:25:06 +0530

    project done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+import Product from "./Components/Product"
+
+
+const App =() =>{
+  return(
+        <>
+        <Product/>
+        </>
+  )
+}
+
+
+export default App;
