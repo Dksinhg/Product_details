efe0d0f83ac1437d9328d8786109ad0bb29cf82d
Dinesh Kumar <ds8284927561@gmail.com>
2021-12-18 15:21:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import ExpenseItem from "./Components/Expenses/ExpenseItem"
+import ExpenseDate from "./Components/Expenses/ExpenseDate";
+import Expenses from "./Components/Expenses/Expenses";
+import Newexpense from "./Components/Newexpense/Newexpense"
+import About from './Components/About'
 
 
-import Product from "./Products"
+
+let DUMMY_EXPENSE = [
+  {
+    id: 'e1',
+    title: 'School fee',
+    amount: 250,
+    date: new Date
+  },
+
+  {
+    id: 'e2',
+    title: 'Books fee',
+    amount: 500,
+    date: new Date
+  },
+
+  {
+    id: 'e3',
+    title: 'House rent',
+    amount: 200,
+    date: new Date
+  },
+
+  {
+    id: 'e4',
+    title: 'Food',
+    amount: 520,
+    date: new Date
+  }
+]
 
 
-const App =() =>{
-  return(
-        <>
-        <Product/>
-        </>
+const App = () => {
+
+  const [expense, setexpense] = useState(DUMMY_EXPENSE);
+
+  const saveExpenseDataHandle = (enteredExpenseData) => {
+    const updatedExpense = [enteredExpenseData, ...expense]
+    setexpense(updatedExpense)
+
+  }
+
+  return (
+
+    <div>
+      <About onSaveExpenseData={saveExpenseDataHandle} />
+      {/* <Newexpense /> */}
+      <Expenses item={expense} />
+
+    </div>
   )
 }
-
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import ReactDom from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
+import './index.css'
+
 
 
 
 ReactDom.render( 
-     <App /> , 
+     <BrowserRouter>
+     <App /> 
+     </BrowserRouter>, 
     
     document.getElementById('root')
     );
